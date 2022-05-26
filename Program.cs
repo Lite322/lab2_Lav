6f24b315333235caa737903b231152c61ed0ba7f
Lite <artem.artamonov.94@bk.ru>
2022-05-26 23:39:13 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab2_Lav
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для нахождения суммарного и среднего объёма n-ого колличества шаров с радиусами r, r + 0.2, r + 0.4, r + 0.6,...");
+            Console.WriteLine("Задайте коллечество шаров:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Задайте начальный радиус:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Суммарный объём:");
+            Console.WriteLine(Volume.Sum(n, r));
+            Console.WriteLine("Средний объём:");
+            Console.WriteLine(Volume.Average(n, r));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Volume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_Lav
+{
+    public class Volume
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"> Колличество шаров </param>
+        /// <param name="r"> Радиус первого шара </param>
+        /// <returns></returns>
+        public static double Sum(int n, double r)
+        {
+            double z = 0;
+            for (int i = 0; i < n; i++)
+            {
+                z += (4 / 3) * Math.PI * Math.Pow(r, 3);
+                r += 0.2;
+            }
+            return z;
+        }
+
+        public static double Average(int n, double r)
+        {
+            double z = Volume.Sum(n, r) / n;
+            return z;
+        }
+    }
+}
