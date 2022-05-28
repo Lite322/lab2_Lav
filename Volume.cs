4da9e63b7a4144440c991f599801d5722b5cf5cd
Lite <artem.artamonov.94@bk.ru>
2022-05-28 23:52:27 +0400

    Final

--- Volume.cs
+++ Volume.cs
@@ -9,7 +9,7 @@ namespace lab2_Lav
     public class Volume
     {
         /// <summary>
-        /// 
+        /// Находит сумму объёмов шаров с радиусами r, r + 0.2, r + 0.4, r + 0.6,...
         /// </summary>
         /// <param name="n"> Колличество шаров </param>
         /// <param name="r"> Радиус первого шара </param>
@@ -19,16 +19,22 @@ namespace lab2_Lav
             double z = 0;
             for (int i = 0; i < n; i++)
             {
-                z += (4 / 3) * Math.PI * Math.Pow(r, 3);
+                z += (4 * Math.PI * Math.Pow(r, 3)) / 3;
                 r += 0.2;
             }
-            return z;
+            return Math.Round(z, 3); // округление до 3 знаков после запятой
         }
 
+        /// <summary>
+        /// Находит среднее значение суммы объёмов шаров
+        /// </summary>
+        /// <param name="n"> Колличество шаров </param>
+        /// <param name="r"> Радиус первого шара </param>
+        /// <returns></returns>
         public static double Average(int n, double r)
         {
             double z = Volume.Sum(n, r) / n;
-            return z;
+            return Math.Round(z, 3); // округление до 3 знаков после запятой
         }
     }
 }
